c5f8df3b34dbad2a10d4d7adec86cabcc477fa17
Tiago Frazão <tiago.frazaod@gmail.com>
2026-03-21 08:42:01 -0300

    Configurações do swagger

--- src/DevIO.Api/Program.cs
+++ src/DevIO.Api/Program.cs
@@ -6,11 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<MeuDbContext>(options =>
 {
@@ -22,7 +20,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
